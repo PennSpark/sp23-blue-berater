5f26c5c13c37448dd3be2d3327d828b0f99f6485
Zain Khan <zain6khan@gmail.com>
2023-04-09 11:46:36 -0400

    Updated models file (and others)

--- backend/berater/beraterapp/views.py
+++ backend/berater/beraterapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.order_by('published_date')
+    return render(request, 'beraterapp/task_list.html', {'tasks': tasks})
