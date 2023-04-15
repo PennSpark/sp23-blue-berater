5b1d1b8b9f3509cfbdceb48fe052c8340ff17c08
sanyashetty <shettysanya@gmail.com>
2023-04-15 16:12:50 -0400

    removed add task

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen'
 import TaskPage from './src/screens/TaskPage.js'
-import AddTaskPage from './src/screens/AddTaskPage.js'
 import HomePage from './src/screens/HomePage';
 import WelcomePage from './src/screens/WelcomePage.js'
 import SignUpPage from './src/screens/WelcomePage.js'
@@ -17,10 +16,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome Page'>
+      <Stack.Navigator initialRouteName='Tasks'>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Task Page" component={TaskPage} />
-        <Stack.Screen name="Add Task" component={AddTaskPage} />
+        <Stack.Screen name="Tasks" component={TaskPage} />
         <Stack.Screen name="Welcome Page" component={WelcomePage} />
         <Stack.Screen name="Sign Up" component={SignUpPage} />
         <Stack.Screen name="Profile" component={ProfilePage} />
