4f79ac31b3b6be620c84ca399b149c127e651b9d
Zain Khan <zain6khan@gmail.com>
2023-04-16 14:32:38 -0400

    Added new views

--- backend/berater/beraterapp/urls.py
+++ backend/berater/beraterapp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.create_task, name="create_task"),
-    path('', views.create_insult, name="create_insult"),
+    path('', views.beraterapp_view, name='beraterapp_view'),
+    # path('', views.create_task, name="create_task"),
+    # path('', views.create_insult, name="create_insult"),
+    path('splash/', views.splash_view, name='splash_view'),
     path('login/', views.login_view, name='login_view'), 
     path('signup/', views.signup_view, name='signup_view'), 
     path('logout/', views.logout_view, name='logout_view'),
-
+    path('delete/', views.delete_view, name='delete_view'),
 ]
